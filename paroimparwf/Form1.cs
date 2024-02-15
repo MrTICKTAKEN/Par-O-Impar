38bc3722a1bdca8dfc67abb1e1949a6d93361c68
MrTICKTAKEN <ticktaken25@gmail.com>
2024-02-15 09:38:53 -0600

    Agregar archivos de proyecto.

---
+++ paroimparwf/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace paroimparwf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1R_Click(object sender, EventArgs e)
+        {
+            int numero;
+            numero = Convert.ToInt32(textBox1.Text);
+            if (numero % 2 == 0)
+            {
+                MessageBox.Show("El numero es par");
+            }
+            else
+            {
+                MessageBox.Show("El numero es impar");
+            }
+        }
+    }
+}
